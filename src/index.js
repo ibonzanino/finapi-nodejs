eb0a87721635431abeb09bb879c5c3b454249544
ibonzanino <bonzaninoigor@gmail.com>
2022-05-27 21:25:21 -0300

    create an account

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-server.listen(3333, () => {
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return res.status(201).send();
+
+});
+
+app.listen(3333, () => {
   console.log('listening on port http://localhost:3333');
 });
