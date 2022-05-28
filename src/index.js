f464d769db589c0811d5f9095fde1c8268fd5558
ibonzanino <bonzaninoigor@gmail.com>
2022-05-27 21:43:32 -0300

    validating account

--- src/index.js
+++ src/index.js
@@ -34,10 +34,14 @@ app.post('/account', (req, res) => {
 
 });
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params;
+app.get("/statement", (req, res) => {
+  const { cpf } = req.headers;
 
   const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ message: 'Customer not found' });
+  }
 
   return res.json(customer.statement);
 });
